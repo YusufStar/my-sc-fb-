48742785008dd8e7d0749217d568cd6a09d42536
Yusuf Yıldız <ysfyldz0077@gmail.com>
2023-02-03 21:45:41 +0300

    my-sc backend

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+const cors = require("cors");
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
+
+dotenv.config();
+
+mongoose.connect(
+  "mongodb+srv://yusuf:jEDkKqy3Q5fPtmBQ@cluster0.rokuwwc.mongodb.net/?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
